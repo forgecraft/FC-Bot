3fbaab9b357846815623c58b655bbcbbd381c8e1
Michael Hillcox <michaelhillcox@gmail.com>
2024-07-12 12:21:47 +0100

    feat: publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,19 @@
+import java.net.URI
+
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.1.0"
     idea
+    `maven-publish`
+    id("me.modmuss50.mod-publish-plugin") version "0.5.2"
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
 }
+
+version = project.properties["version"] as String
 
 sourceSets {
     main {
@@ -38,3 +44,51 @@ tasks {
     }
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "net.forgecraft.services"
+            from(components["java"])
+        }
+    }
+    repositories {
+        val token = providers.environmentVariable("GITHUB_TOKEN")
+        if (token.isPresent) {
+            maven {
+                name = "GitHubPackages"
+                url = URI("https://maven.pkg.github.com/forgecraft/FC-Bot")
+                credentials {
+                    username = providers.environmentVariable("GITHUB_ACTOR").get()
+                    password = token.get()
+                }
+            }
+        }
+
+        val sapsToken = providers.environmentVariable("SAPS_TOKEN")
+        if (sapsToken.isPresent) {
+            maven {
+                name = "Mikey's Saps Maven Repository"
+                url = URI("https://maven.saps.dev/releases")
+                credentials {
+                    username = "forgecraft"
+                    password = sapsToken.get()
+                }
+            }
+        }
+    }
+}
+
+publishMods {
+    dryRun = providers.environmentVariable("GITHUB_TOKEN").getOrNull() == null
+    file = tasks.shadowJar.get().archiveFile.get().asFile
+    additionalFiles.from(tasks.jar.get().archiveFile.get().asFile)
+    changelog = ""
+    type = STABLE
+
+    github {
+        repository = "forgecraft/FC-Bot"
+        accessToken = providers.environmentVariable("GITHUB_TOKEN")
+        commitish = providers.environmentVariable("GITHUB_SHA").orElse("dryRun")
+        tagName = providers.environmentVariable("GITHUB_REF_NAME").orElse("dryRun")
+    }
+}
